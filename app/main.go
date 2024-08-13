28d721228f5b236a27768b6c17183ce0c00002d7
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-13 15:50:58 +0300

    Implement worker pool.

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	const numJobs = 4
+	const numJobs = 1
 	const numWorkers = 2
 
 	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
@@ -16,7 +16,19 @@ func main() {
 	defer close(videoQueue)
 
 	wp := streamer.New(videoQueue, numWorkers)
-	fmt.Println("wp:", wp)
 
 	wp.Run()
+	fmt.Println("Worker pool started. Press enter to continue.")
+	_, _ = fmt.Scanln()
+
+	video := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
+
+	videoQueue <- streamer.VideoProcessingJob{Video: video}
+
+	for i := 1; i < numJobs; i++ {
+		msg := <-notifyChan
+		fmt.Println(msg)
+	}
+
+	fmt.Println("Done!")
 }
