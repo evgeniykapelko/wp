b257b1d904d09e824a79c5388607be27774a6da7
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-13 17:56:48 +0300

    Implement worker pool.

--- app/main.go
+++ app/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	const numJobs = 1
-	const numWorkers = 2
+	const numJobs = 4
+	const numWorkers = 4
 
 	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
 	defer close(notifyChan)
@@ -18,12 +18,25 @@ func main() {
 	wp := streamer.New(videoQueue, numWorkers)
 
 	wp.Run()
-	fmt.Println("Worker pool started. Press enter to continue.")
-	_, _ = fmt.Scanln()
 
-	video := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
+	video1 := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
 
-	videoQueue <- streamer.VideoProcessingJob{Video: video}
+	video2 := wp.NewVideo(2, "./input/bad.txt", "./output", "mp4", notifyChan, nil)
+	ops := &streamer.VideoOptions{
+		RenameOutput:    true,
+		SegmentDuration: 10,
+		MaxRate1080p:    "1200k",
+		MaxRate720p:     "600K",
+		MaxRate480p:     "400K",
+	}
+	video3 := wp.NewVideo(3, "./input/puppy2.mp4", "./output", "hls", notifyChan, ops)
+
+	video4 := wp.NewVideo(4, "./input/puppy2.mp4", "./output", "mp4", notifyChan, nil)
+
+	videoQueue <- streamer.VideoProcessingJob{Video: video1}
+	videoQueue <- streamer.VideoProcessingJob{Video: video2}
+	videoQueue <- streamer.VideoProcessingJob{Video: video3}
+	videoQueue <- streamer.VideoProcessingJob{Video: video4}
 
 	for i := 1; i < numJobs; i++ {
 		msg := <-notifyChan
--- streamer/streamer.go
+++ streamer/streamer.go
@@ -1,5 +1,13 @@
 package streamer
 
+import (
+	"fmt"
+	"github.com/tsawler/toolbox"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
 type ProcessingMessage struct {
 	ID         int
 	Successful bool
@@ -21,11 +29,111 @@ type Video struct {
 	OutputDir    string
 	EncodingType string
 	NotifyChan   chan ProcessingMessage
+	Options      *VideoOptions
 	Encoder      Processor
 }
 
-func (v *Video) encode() {
+type VideoOptions struct {
+	RenameOutput    bool
+	SegmentDuration int
+	MaxRate1080p    string
+	MaxRate720p     string
+	MaxRate480p     string
+}
 
+func (vd *VideoDispatcher) NewVideo(id int, input, output, encType string, notifyChan chan ProcessingMessage, options *VideoOptions) Video {
+	if options == nil {
+		options = &VideoOptions{}
+	}
+
+	fmt.Println("New video created:", id, input)
+
+	return Video{
+		ID:           id,
+		InputFile:    input,
+		OutputDir:    output,
+		EncodingType: encType,
+		NotifyChan:   notifyChan,
+		Encoder:      vd.Processor,
+		Options:      options,
+	}
+}
+
+func (v *Video) encode() {
+	var fileName string
+
+	switch v.EncodingType {
+	case "mp4":
+		fmt.Println("v.encode(): About to encode to mp4", v.ID)
+		name, err := v.encodeToMP4()
+		if err != nil {
+			// send information to the notifyChan
+			v.sendToNotifyChan(false, "", fmt.Sprintf("Error encoding to MP4: %s", err))
+			return
+		}
+		fileName = fmt.Sprintf("%s.mp4", name)
+	case "hls":
+		name, err := v.encodeToHLS()
+		if err != nil {
+			// send information to the notifyChan
+			v.sendToNotifyChan(false, "", fmt.Sprintf("Error encoding to MP4: %s", err))
+			return
+		}
+		fileName = fmt.Sprintf("%s.m3u8", name)
+	default:
+		v.sendToNotifyChan(false, "", fmt.Sprintf("Encoding type not supported: %s", v.EncodingType))
+		return
+	}
+
+	v.sendToNotifyChan(true, fileName, fmt.Sprintf("Encoding type %s", v.EncodingType))
+}
+
+func (v *Video) encodeToMP4() (string, error) {
+	baseFileName := ""
+
+	if !v.Options.RenameOutput {
+		b := path.Base(v.InputFile)
+		baseFileName = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		var t toolbox.Tools
+		baseFileName = t.RandomString(10)
+
+	}
+
+	err := v.Encoder.Engine.EncodeToMP4(v, baseFileName)
+	if err != nil {
+		return "", err
+	}
+
+	return baseFileName, nil
+}
+
+func (v *Video) encodeToHLS() (string, error) {
+	baseFileName := ""
+
+	if !v.Options.RenameOutput {
+		b := path.Base(v.InputFile)
+		baseFileName = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		var t toolbox.Tools
+		baseFileName = t.RandomString(10)
+	}
+
+	err := v.Encoder.Engine.EncodeToHLS(v, baseFileName)
+	if err != nil {
+		return "", err
+	}
+
+	return baseFileName, nil
+}
+
+func (v *Video) sendToNotifyChan(successful bool, fileName string, message string) {
+	v.NotifyChan <- ProcessingMessage{
+		ID:         v.ID,
+		Successful: successful,
+		Message:    message,
+		OutputFile: fileName,
+	}
 }
 
 func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
